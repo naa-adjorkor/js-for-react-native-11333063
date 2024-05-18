5639b82465b0afaa39129a2046d26daf298003eb
naa-adjorkor <naakrong_adjei@st.ug.edu.gh>
2024-05-18 18:35:31 +0000

    added formatArrayStrings function

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,9 +1,24 @@
 //array of numbers
 
 const numbers =[3, 10, 5, 22, 19, 28];
+const strings =["Bernice", "Bernard", "Ophelia", "Cherry","Thelma", "Juliana"];
 
 //Task 1: processArray function
 function processArray(numbers){
     return numbers.map(num => num%2 === 0? num*num : num*3);
 }
+
+//Task 2 : formatArrayStrings
+function formatArrayStrings(strings,numbers){
+    return strings.map((str, index)=>{
+        if (numbers[index] % 2 === 0){
+            return str.toUpperCase();
+        }else{
+            return str.toLowerCase();
+        }
+        });
+    }
+
+
+console.log(formatArrayStrings(strings,numbers));
 console.log(processArray(numbers));
